e0a316391a2af6e0305f3f5cb4fd53fa8f59e187
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2025-02-13 14:47:55 +0800

    add upload file

--- loading_TP_handler.py
+++ loading_TP_handler.py
@@ -1,13 +1,17 @@
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.types import Message, File
+from aiogram.types import Message, InlineKeyboardButton, CallbackQuery
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from utils.L2.loading_tp_statistical_card import upload_statistic_card_from_ECP
 
 router = Router()
 
 
 class LoadingHandler(StatesGroup):
     step_1 = State()
+    step_2 = State()
 
 
 @router.message(F.text == 'Отправить отчет по травмпункту')
@@ -21,4 +25,16 @@ async def info_loading_handler(message: Message, state: FSMContext):
 @router.message(LoadingHandler.step_1)
 async def get_file(message: Message, state: FSMContext):
     await message.bot.download(file=message.document.file_id, destination=f'table/{message.document.file_name}')
+    builder = InlineKeyboardBuilder()
+    builder.row(InlineKeyboardButton(text='Да', callback_data='1'))
+    builder.row(InlineKeyboardButton(text='Нет', callback_data='0'))
+    await message.answer('Отчет загружен, выгрузить в ЕЦП', reply_markup=builder.as_markup())
+    await state.set_state(LoadingHandler.step_2)
+
+
+@router.callback_query(LoadingHandler.step_2)
+async def loading_patients(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer('Начал выгружать в ЕЦП')
+    upload_statistic_card_from_ECP()
+    await callback.message.answer('Закончил выгружать в ЕЦП')
     await state.clear()
