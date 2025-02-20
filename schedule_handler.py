f079bc627b7bd23fc572895d37ae197b18a6e1ca
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2025-02-20 09:28:06 +0800

    fix scheduler

--- schedule_handler.py
+++ schedule_handler.py
@@ -1,17 +1,15 @@
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
 
-from settings import admin
 from utils.L2.diaries import create_diaries_function
 
 router = Router()
 
 
 @router.message(Command(commands=['scheduler']))
-async def start_scheduler(message: Message):
-    await message.bot.send_message(chat_id=admin, text='Запущен процесс создания дневников')
+async def start_scheduler():
+    print('Запущен процесс создания дневников')
     if create_diaries_function():
-        await message.bot.send_message(chat_id=admin, text='Дневники созданы, проверяй')
+        print('Дневники созданы, проверяй')
     else:
-        await message.bot.send_message(chat_id=admin, text='Что-то пошло не так')
+        print('Что-то пошло не так')
