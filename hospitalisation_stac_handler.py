7423c5cd12f29a37c2f99c6285cee9f2abb9e5dd
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2025-04-14 20:15:49 +0800

    обновление выгрузки в ЕЦП: добавление пациентов в текущие, при выписке - протокол операции, выписной эпикриз (дополнение)

--- hospitalisation_stac_handler.py
+++ hospitalisation_stac_handler.py
@@ -3,9 +3,11 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
+from utils.ECP.stationar_v2 import working_with_stories
+
 router = Router()
 
 
 @router.message(Command(commands=['hospitalize']))
-async def hospitalize(message: Message, state: FSMContext):
+async def hospitalize(message: Message):
     working_with_stories()
